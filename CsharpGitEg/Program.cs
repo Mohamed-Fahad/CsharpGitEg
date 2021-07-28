fa917bd2541f206157cd1250b7a615db807b9bd3
Mohamed-Fahad <mamohamedfahad@gmail.com>
2021-07-28 11:08:26 +0530

    Add project files.

---
+++ CsharpGitEg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpGitEg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
